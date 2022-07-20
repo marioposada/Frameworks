a4d1baa30f08a03f783f3650660fd8a4c3bf879c
unknown <marioposadamiranda@gmail.com>
2022-07-19 22:25:12 -0300

    Material UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MaterialUI from "./components/MaterialUI";
+
+function App() {
+  return (
+    <div>
+      <MaterialUI />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MaterialUI.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PrimarySeachAppBar  from "./PrimarySearchAppBar"
+
+
+export default function MaterialUI () {
+    return(
+        
+        <PrimarySeachAppBar />
+    )
+}
